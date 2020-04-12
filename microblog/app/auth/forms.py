776d4cbb485476c76c31675da0bc36453444aa70
osboxes.org <190337259@stu.vtc.edu.hk>
2020-04-12 13:09:47 -0400

    second commit

--- microblog/app/auth/forms.py
+++ microblog/app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_babel import lazy_gettext as _l, _
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError, IntegerField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 from app.models import User
@@ -14,8 +14,11 @@ class LoginForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
+    first_name = StringField(_l('First Name'), validators=[DataRequired()])
+    last_name = StringField(_l('Last Name'), validators=[DataRequired()])
     username = StringField(_l('Username'), validators=[DataRequired()])
     email = StringField(_l('Email'), validators=[DataRequired(), Email()])
+    phone = IntegerField(_('Phone'), validators=[DataRequired()])
     password = PasswordField(_l('Password'), validators=[DataRequired()])
     password2 = PasswordField(
         _l('Repeat Password'), validators=[DataRequired(),
@@ -31,6 +34,11 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError(_('Please use a different email address.'))
+
+    def validate_phone_number(self, phone):
+        user = User.query.filter_by(phone=phone.data).first()
+        if user is not None:
+            raise ValidationError(_('Please use a different phone number.'))
 
 
 class ResetPasswordRequestForm(FlaskForm):
